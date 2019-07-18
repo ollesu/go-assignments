8aac6a46f285e516d3518d735a65bace5afc1122
Aisa Mukukenova <mkaysa@gmail.com>
2019-07-18 17:31:49 +0100

    finish reader & writer

--- http/main.go
+++ http/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct{}
 
 func main() {
 	resp, err := http.Get("http://google.com")
@@ -13,11 +16,25 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Reader pushes raw data response into this byte slice
-	byteSlice := make([]byte, 99999)
-	resp.Body.Read(byteSlice)
+	// Read func pushes raw body response into this byte slice
+	// 1) byteSlice := make([]byte, 99999)
+	// resp.Body.Read(byteSlice)
+	// fmt.Println(string(byteSlice))
+
+	lw := logWriter{}
+	// writer interface, reader interface
+	io.Copy(lw, resp.Body)
+}
+
+// logWriter now implements Writer interface!
+func (logWriter) Write(byteSlice []byte) (int, error) {
 	fmt.Println(string(byteSlice))
+
+	fmt.Println("Just wrote this many bytes:", len(byteSlice))
+	return len(byteSlice), nil
 }
 
 // Reader interface handles all different sources of data (http request, text file, user input from cli, coming into the programme
-// it translates it into the common output - []byte
+// it translates it into the common output data - []byte
+
+// every source of data (eg http request body, user input) implements a Reader
