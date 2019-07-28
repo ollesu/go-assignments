c74a100c637fafdb7db3158302bf3557be7d08cb
Aisa Mukukenova <mkaysa@gmail.com>
2019-07-28 21:52:13 +0100

    practising channels and goroutines

---
+++ channels/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+
+	for _, link := range links {
+		go checkStatus(link)
+	}
+}
+
+func checkStatus(link string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down!")
+		return
+	}
+
+	fmt.Println(link, "is ok.")
+}
+
+/* goroutines
+	- engine that executes code line by line
+	- launching additional go routines 
+	- 
+/* 
